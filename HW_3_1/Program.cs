e9839365da0afd9a395b9b00f0fefda828a89f9d
Ihor.Lyman <laim0999716349@gmail.com>
2023-01-12 01:09:05 +0200

    version7.0

---
+++ HW_3_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 10, 12, 13, 14 };
+
+            list.Add(12);
+
+            list.Remove(13);
+
+            list[5] = 10;
+
+            list.AddRange(new int[] { 1, 2, 3, 4 });
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            list.Sort();
+
+            Console.WriteLine();
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            
+        }
+    }
+}
---
+++ HW_3_2/BinaryTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW_3_2
+{
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T> 
+    {
+        public IEnumerable<T> GetPreOrder { get => First?.GetPreOrder(); }
+        public IEnumerable<T> GetPostOrder { get => First?.GetPostOrder(); }
+        private BinaryNode<T>? First { get; set; }
+
+        public void Add(T item)
+        {
+            if (First is null)
+                First = new(item);
+            else
+                First.Add(item);
+        }
+        public bool Remove(T item)
+        {
+            if (First.Value.CompareTo(item) == 0)
+            {
+                BinaryNode<T> left = First.Left;
+                BinaryNode<T> right = First.Right;
+                if(left != null)
+                {
+                    First = left;
+                    First.AddNode(right);
+                }
+                else
+                {
+                    First = right;
+                }
+                return true;
+            }
+            else
+            {
+                BinaryNode<T> node = First.Remove(item);
+                if (node == null)
+                    return false;
+                if (node.Left != null)
+                    First.AddNode(node.Left);
+                if (node.Right != null)
+                    First.AddNode(node.Right);
+
+                return true;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in First.GetInOrder())
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
